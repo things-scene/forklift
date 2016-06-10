ec8decc3c5a3ff828215164b09d39f95e79e931b
Lee-YuMin <lym@hatiolabs-Mac-mini.local>
2016-06-10 15:24:50 +0900

    히든 수정

--- src/forklift.js
+++ src/forklift.js
@@ -4,36 +4,28 @@ export default class Forklift extends Ellipse {
 
   _draw(context) {
     var {
-      imageNumber = 1,
-      hidden = false,
-      fillStyle = '#389fdb',
       cx,
       cy,
       rx,
       ry
     } = this.model;
 
-    if(!hidden){
+    context.beginPath()
+    context.ellipse(cx, cy, rx, ry, 0, 0, Math.PI * 2)
+    context.fillStyle = '#389fdb'
+    context.fill()
 
-      context.beginPath()
-      context.ellipse(cx, cy, rx, ry, 0, 0, Math.PI * 2)
-      context.fillStyle = '#389fdb'
-      context.fill()
+    context.beginPath()
+    context.ellipse(cx, cy, rx, ry, 0, 0, Math.PI * 2)
 
-      context.beginPath()
-      context.ellipse(cx, cy, rx, ry, 0, 0, Math.PI * 2)
-
-      this.model.fillStyle = {
-        type: 'pattern',
-        fitPattern: true,
-        image: '../images/forklift.png'
-        // image: '../images/' + imageNumber + '.png'
-      }
-      this.drawFill(context)
+    this.model.fillStyle = {
+      type: 'pattern',
+      fitPattern: true,
+      image: '../images/forklift.png'
+      // image: '../images/' + imageNumber + '.png'
     }
+    this.drawFill(context)
   }
-
-  get controls(){}
 }
 
 Component.register('forklift', Forklift)
